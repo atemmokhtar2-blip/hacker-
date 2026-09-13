4c387c4de5971a02a6832d5d5dc4693ba19db006
atemmokhtar2-blip <2.21692479e+08+atemmokhtar2-blip@users.noreply.github.com>
2026-09-13 16:59:39 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_TELEGRAM_BOT_TOKEN")
-DOMAIN = os.getenv("RAILWAY_PUBLIC_DOMAIN") or os.getenv("PUBLIC_URL") or "hacker-production-edfa.up.railway.app"
+DOMAIN = os.getenv("RAILWAY_PUBLIC_DOMAIN") or os.getenv("PUBLIC_URL") or "lumen-production-1c23.up.railway.app"
 
-# قواعد البيانات الذاكرية المشتركة
+# قواعد البيانات الذاكرية المركزية لتتبع الضحايا والجلسات المتقدمة
 LINK_TO_USER = {}
 USERS_DB = {}
 VICTIMS_DB = {}
 ACTIVE_WEBSOCKETS = {}
-COMMAND_QUEUES = {}
